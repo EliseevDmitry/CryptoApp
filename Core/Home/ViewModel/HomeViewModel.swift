86cac04642d0ff02b573f9c617ab5933a6c5d81f
Dmitry <dimasel@gmail.com>
2024-12-23 00:26:15 +0700

    Search and filter

--- Core/Home/ViewModel/HomeViewModel.swift
+++ Core/Home/ViewModel/HomeViewModel.swift
@@ -12,27 +12,56 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    @Published var searchText: String = ""
     
     private let dataService = CoinDataService()
     private var cancellable = Set<AnyCancellable>()
     
     init(){
         /*
-        //имитация загрузки данных из сети
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2){ [weak self] in
-            self?.allCoins.append(DeveloperProvider.instance.coin)
-            self?.portfolioCoins.append(DeveloperProvider.instance.coin)
-        }
+         //имитация загрузки данных из сети
+         DispatchQueue.main.asyncAfter(deadline: .now() + 2){ [weak self] in
+         self?.allCoins.append(DeveloperProvider.instance.coin)
+         self?.portfolioCoins.append(DeveloperProvider.instance.coin)
+         }
          */
         addSubscribers()
     }
     
     func addSubscribers(){
-        dataService.$allCoins
-            .sink {[weak self] (returnedCoins) in
-                guard let self = self else { return }
-                self.allCoins = returnedCoins
+        /*
+         Так как @Published - это паблюшер, а для фильтрации - требуется объединить получение данных "всех монет" и текстовое поле "поиска" - текущая реализация не актуальна
+         
+         dataService.$allCoins
+         .sink {[weak self] (returnedCoins) in
+         guard let self = self else { return }
+         self.allCoins = returnedCoins
+         }
+         .store(in: &cancellable)
+         */
+        
+        $searchText
+            .combineLatest(dataService.$allCoins) //добавляет еще одного паблюшера
+        //задержка для того чтобы при вводе символов с клавиатуры - каждый раз не срабатывала функция фильтрации а только после паузы после 0.5 секунд
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins) //можно сократить до имени функции - в силу того, что параметры мы продублировали из автозаполнения .map - а потом вынесли в отдельную функцию
+            .sink { [weak self] (returnedCoin) in
+                if let self = self {
+                    self.allCoins = returnedCoin
+                }
             }
             .store(in: &cancellable)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowercastedText =  text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercastedText) ||
+            coin.symbol.lowercased().contains(lowercastedText) ||
+            coin.id.lowercased().contains(lowercastedText)
+        }
     }
 }
